425649eb5768d07832b751b1a0ae7dd4bed29b71
Lokesh Gurung <lokeshgurungwj@gmail.com>
2020-11-28 21:18:27 +0545

    Created model and route for posts

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const postRoutes = require("./routes/posts");
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 const dbUrl = process.env.DB_URL;
+
+app.use(express.json());
 
 // Connecting to MongoDB
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected!"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-    res.send("Namaste from the server!");
-});
+app.use("/posts", postRoutes);
 
 app.listen(port, () => {
     console.log(`The server is running at port ${port}.`);
---
+++ models/Posts.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const PostModel = mongoose.model("Post", PostSchema);
+
+module.exports = PostModel;
